952ee5b7916f34f54569a5134a50a0ac141ea3fa
Salmandotweb <salmandotweb@gmail.com>
2023-09-25 01:37:43 +0500

    adding stripe webhooks

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,9 +3,9 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(
     process.env.STRIPE_SECRET_KEY ?? '',
     {
-        apiVersion: '2023-08-16',
+        apiVersion: "2023-08-16",
         appInfo: {
-            name: 'AI2SaaS',
+            name: 'ai2saas',
             version: '0.1.0'
         }
     }
