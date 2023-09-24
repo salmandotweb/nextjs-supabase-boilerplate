b9192b8f170db69b9e613dbe30eaf680bb2aabb9
Salmandotweb <salmandotweb@gmail.com>
2023-09-25 00:52:58 +0500

    adding user provider

---
+++ src/providers/UserProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { MyUserContextProvider } from "@/hooks/useUser";
+
+interface UserProviderProps {
+	children: React.ReactNode;
+}
+
+const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+	return <MyUserContextProvider>{children}</MyUserContextProvider>;
+};
+
+export default UserProvider;
