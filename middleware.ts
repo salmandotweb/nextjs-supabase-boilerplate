1af1de9809e5dd6b69269f4b58cddd93fb98c92a
Salmandotweb <salmandotweb@gmail.com>
2023-09-25 22:42:35 +0500

    implementing stripe with products activation

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+    const res = NextResponse.next();
+    const supabase = createMiddlewareClient({ req, res });
+    await supabase.auth.getSession();
+    return res;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
+"use client";
+
 import { ModeToggle } from "@/components/theme-toggle";
+import { useUser } from "@/hooks/useUser";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+import {
+	useSessionContext,
+	useSupabaseClient,
+} from "@supabase/auth-helpers-react";
 
 export default function Home() {
+	const { user } = useUser();
+	const { session } = useSessionContext();
+
+	const supabaseClient = useSupabaseClient();
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<ModeToggle />
+			{user ? (
+				<ModeToggle />
+			) : (
+				<div className="w-[300px]">
+					<Auth
+						supabaseClient={supabaseClient}
+						providers={["github"]}
+						magicLink={true}
+						appearance={{
+							theme: ThemeSupa,
+							variables: {
+								default: {
+									colors: {
+										brand: "#404040",
+										brandAccent: "#22c55e",
+									},
+								},
+							},
+						}}
+						theme="dark"
+					/>
+				</div>
+			)}
 		</main>
 	);
 }
