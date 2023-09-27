670d633c6349c423ddd4769486583a8fb346e973
Salmandotweb <salmandotweb@gmail.com>
2023-09-28 01:19:20 +0500

    adding hero image

--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const Header = () => {
 	return (
-		<div className="w-full min-h-[80vh] flex items-center justify-center flex-col text-center relative overflow-hidden gap-6">
+		<div className="w-full h-[93vh] flex items-center justify-center flex-col text-center relative overflow-hidden gap-6 pt-[500px]">
 			<h1 className="text-7xl font-bold leading-snug">
 				All your business <br />
 				expenses in one place.
@@ -11,10 +12,17 @@ const Header = () => {
 				Your one-stop finance empower platform. <br /> Manage all your business
 				expenses with our supafast app.
 			</p>
-			<div className="flex items-center justify-center gap-6">
+			<div className="flex items-center justify-center gap-6 mb-[50px]">
 				<Button variant="default">Get a Free Demo</Button>
 				<Button variant="secondary">See Pricing</Button>
 			</div>
+
+			<Image
+				src="/images/Dashboard.png"
+				alt="Dashboard"
+				width={1200}
+				height={852}
+			/>
 		</div>
 	);
 };
